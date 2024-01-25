452522d0e416ce391aeff6cac10bab3d6e235576
vagrant <pihugupta0501@gmail.com>
2024-01-25 02:48:31 +0000

    add a post route to login, create a cookie and set its value to the username submitted in the form, redirect user to /urls after login

--- express_server.js
+++ express_server.js
@@ -82,6 +82,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+
+res.cookie('username', username);
+res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
